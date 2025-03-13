2f1eef85f4c20b8b8a46cc786ed0e71369426951
K0ki4 <nikitad_a_v@mail.ru>
2025-03-13 19:29:56 +0300

    Job

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import datetime
+from xmlrpc.client import DateTime
+
 from flask import Flask
 from data import db_session
+from data.jobs import Jobs
 from data.user import User
 
 app = Flask(__name__)
@@ -23,6 +27,18 @@ def add_capitan():
     user.hashed_password = 'capitan'
     db_sess = db_session.create_session()
     db_sess.add(user)
+    db_sess.commit()
+
+def first_job():
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+    db_sess = db_session.create_session()
+    db_sess.add(job)
     db_sess.commit()
 
 
